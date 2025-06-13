ea434c5a05754c6e5159af9dd8c8024713f51ddc
daguri1985 <daguri1985@gmail.com>
2025-06-13 16:19:24 +0300

    pagination latest

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,12 +1,23 @@
 const Pagination = () =>{
     return (
-        <div className="flex items-center justify-between p-4">
-            <span className="text-gray-500">Showing 1-10 of 50</span>
+        <div className="flex items-center justify-between p-4 tex-gray-700">
+           
             <div className="flex items-center gap-2">
-                <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors">
+                <button disabled className="bg-green-500 text-white text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed px-4 py-2 rounded-md hover:bg-green-600 transition-colors">
                     Previous
                 </button>
-                <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors">
+                 <div className="text-gray-500 flex items-center">
+                    <button className="px-2 bg-red-500  rounded-sm text-white">
+1
+                    </button>
+                     <button className="px-2 rounded-sm">
+2
+                    </button>
+                     <button className="px-2 rounded-sm">
+3
+                    </button>
+                 </div>
+                <button className="bg-red-500 text-white text-xs font-semibold px-4 py-2 rounded-md hover:bg-green-600 transition-colors">
                     Next
                 </button>
             </div>
