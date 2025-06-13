86b507752818c988673cd79004ee2d5d00b57ecf
daguri1985 <daguri1985@gmail.com>
2025-06-13 16:22:50 +0300

    pagination latest centered

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,25 +2,25 @@ const Pagination = () =>{
     return (
         <div className="flex items-center justify-between p-4 tex-gray-700">
            
-            <div className="flex items-center gap-2">
+       
                 <button disabled className="bg-green-500 text-white text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed px-4 py-2 rounded-md hover:bg-green-600 transition-colors">
                     Previous
                 </button>
                  <div className="text-gray-500 flex items-center">
-                    <button className="px-2 bg-red-500  rounded-sm text-white">
-1
+                    <button className="px-2 bg-red-500  rounded-sm text-white">1
                     </button>
-                     <button className="px-2 rounded-sm">
-2
+                     <button className="px-2 rounded-sm">2
                     </button>
-                     <button className="px-2 rounded-sm">
-3
+                     <button className="px-2 rounded-sm">3
+                    </button>
+                    .....
+                      <button className="px-2 rounded-sm">10
                     </button>
                  </div>
                 <button className="bg-red-500 text-white text-xs font-semibold px-4 py-2 rounded-md hover:bg-green-600 transition-colors">
                     Next
                 </button>
-            </div>
+           
         </div>
     );
 }
